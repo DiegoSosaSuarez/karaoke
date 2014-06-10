f16dc754b5fc4f23ca752e4dfc0a31282cc72877
DiegoSosaSuarez <diego.sosa_123@hotmail.com>
2014-06-10 14:58:40 -0500

    Agregue Constructor, geters y seters de la clase Genero.

--- Karaoke_0.1/src/logica/Genero.java
+++ Karaoke_0.1/src/logica/Genero.java
@@ -7,4 +7,34 @@ public class Genero {
 	private String nombre;
 	private String imagenGenero;
 	private ArrayList<String> listaAutores;
+
+	public Genero() {
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getImagenGenero() {
+		return imagenGenero;
+	}
+
+	public void setImagenGenero(String imagenGenero) {
+		this.imagenGenero = imagenGenero;
+	}
+
+	public ArrayList<String> getListaAutores() {
+		return listaAutores;
+	}
+
+	public void setListaAutores(ArrayList<String> listaAutores) {
+		this.listaAutores = listaAutores;
+	}
+	
+	
+	
 }
